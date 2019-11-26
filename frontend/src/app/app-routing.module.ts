e9dde37e7c8e06db4d094708ba6c2e298fa8381f
Scizivire <danilolima@live.nl>
2019-11-26 15:33:59 +0100

    add components for pages

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FindRestaurantComponent } from './find-restaurant/find-restaurant.component';
+import { GroupsComponent } from './groups/groups.component';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'find-restaurant', component: FindRestaurantComponent},
+  {path: 'groups', component: GroupsComponent}
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FindRestaurantComponent } from './find-restaurant/find-restaurant.component';
+import { GroupsComponent } from './groups/groups.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    FindRestaurantComponent,
+    GroupsComponent
   ],
   imports: [
     BrowserModule,
