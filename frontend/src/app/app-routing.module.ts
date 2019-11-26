41baa2db6b3012c696e34cd728740c4cc8107490
Scizivire <danilolima@live.nl>
2019-11-26 14:00:55 +0100

    header

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  {path: 'header', component: HeaderComponent},
   {path: 'home', component: HomeComponent},
 ];
 
