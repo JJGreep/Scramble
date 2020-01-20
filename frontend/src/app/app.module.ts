3df7245c9f0ea4458a2a757fc5dabcf8637e5bab
Scizivire <danilolima@live.nl>
2020-01-20 15:26:45 +0100

    Revert "Merge branch 'Filtering'"
    This reverts commit 35f1e2b0cd993454d37963669476c75422f00911, reversing
    changes made to 49fea5f320ac2928e3a0e5636ea73da383841880.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,16 +9,10 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { FindRestaurantComponent } from './find-restaurant/find-restaurant.component';
-
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
-import { RestaurantComponent } from './restaurant/restaurant.component';
-import { RestaurantlistComponent } from './restaurantlist/restaurantlist.component';
-import { FiltersComponent } from './filters/filters.component';
-import { RestaurantlistpageComponent } from './restaurantlistpage/restaurantlistpage.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {EatgroupListComponent} from "./eatgroup-list/eatgroup-list.component";
-import {EatgroupCreateComponent} from "./eatgroup-create/eatgroup-create.component";
-
+import { EatgroupListComponent } from './eatgroup-list/eatgroup-list.component';
+import { EatgroupCreateComponent } from './eatgroup-create/eatgroup-create.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -29,10 +23,6 @@ import {EatgroupCreateComponent} from "./eatgroup-create/eatgroup-create.compone
     RegisterComponent,
     FindRestaurantComponent,
     RestaurantDetailComponent,
-    RestaurantComponent,
-    RestaurantlistComponent,
-    FiltersComponent,
-    RestaurantlistpageComponent,
     EatgroupListComponent,
     EatgroupCreateComponent
   ],
--- frontend/src/app/res-restaurants.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ResRestaurants } from './res-restaurants';
-
-describe('ResRestaurants', () => {
-  it('should create an instance', () => {
-    expect(new ResRestaurants()).toBeTruthy();
-  });
-});
