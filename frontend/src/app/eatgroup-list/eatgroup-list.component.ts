70e49aa7e53f1cb3e8da3915fa6841fea0c731fa
wdebitewari <wikash@debitewari.nl>
2020-01-27 19:16:20 +0100

    Again and again...

--- frontend/src/app/eatgroup-list/eatgroup-list.component.ts
+++ frontend/src/app/eatgroup-list/eatgroup-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EatgroupService} from "../eatgroup.service";
+import {Eatgroup} from "../eatgroup";
 
 @Component({
   selector: 'app-eatgroup-list',
@@ -7,12 +8,12 @@ import { EatgroupService} from "../eatgroup.service";
   styleUrls: ['./eatgroup-list.component.css']
 })
 export class EatgroupListComponent implements OnInit {
-  eatgroups: any = [];
+  eatgroups: Eatgroup[] = [];
 
   constructor(public eatgroupService: EatgroupService) { }
 
   ngOnInit() {
-    this.eatgroupService.getAll().subscribe((data:{}) => {
+    this.eatgroupService.getAll().subscribe((data) => {
         console.log("EatGroups", data);
         this.eatgroups = data;
       }
